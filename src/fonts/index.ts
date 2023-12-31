9536a4012b87834147401ee36b6d67d1cdabf168
Kai <orozobekov.kai@gmail.com>
2023-12-31 11:18:13 -0500

    feat: initial setup

---
+++ src/fonts/index.ts
@@ -0,0 +1,13 @@
+import { Ubuntu, Rubik } from "next/font/google";
+
+export const rubik = Rubik({
+  display: "swap",
+  variable: "--font-rubik",
+  weight: "400",
+});
+
+export const ubuntu = Ubuntu({
+  display: "swap",
+  variable: "--font-ubuntu",
+  weight: "400",
+});
